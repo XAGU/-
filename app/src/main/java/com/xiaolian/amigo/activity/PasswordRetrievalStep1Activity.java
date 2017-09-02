c72b7897c4ed7e6d7fc0e8d6203bf93799e031a4
caidong <caidong@youzan.com>
2017-09-02 11:49:05 +0800

    加入验证码发送

--- app/src/main/java/com/xiaolian/amigo/activity/PasswordRetrievalStep1Activity.java
+++ app/src/main/java/com/xiaolian/amigo/activity/PasswordRetrievalStep1Activity.java
@@ -1,8 +1,12 @@
 package com.xiaolian.amigo.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +14,7 @@ import com.xiaolian.amigo.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
 /**
@@ -21,7 +26,9 @@ public class PasswordRetrievalStep1Activity extends AppCompatActivity {
     @BindView(R.id.et_mobile)
     TextView et_mobile;
     @BindView(R.id.et_verification_code)
-    TextView et_verification_code;
+    TextView et_verificationCode;
+    @BindView(R.id.bt_send_verification_code)
+    Button bt_sendVerificationCode;
     @BindView(R.id.bt_submit)
     Button bt_submit;
 
@@ -46,12 +53,41 @@ public class PasswordRetrievalStep1Activity extends AppCompatActivity {
         toggleSubmitBtnStatus();
     }
 
+    @OnClick(R.id.et_mobile)
+    void testClickText() {
+        Log.e("===========>", "111");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @OnClick(R.id.bt_send_verification_code)
+    void sendVerificationCode() {
+        final AppCompatActivity context = this;
+        CountDownTimer timer = new CountDownTimer(30000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                bt_sendVerificationCode.setText(millisUntilFinished / 1000 + "秒");
+            }
+
+            @Override
+            public void onFinish() {
+                bt_sendVerificationCode.setEnabled(true);
+                bt_sendVerificationCode.setText("重新获取");
+                bt_sendVerificationCode.setBackground(context.getResources().getDrawable(R.drawable.verification_code_1));
+            }
+        };
+
+        bt_sendVerificationCode.setEnabled(false);
+        timer.start();
+        bt_sendVerificationCode.setBackground(this.getResources().getDrawable(R.drawable.verification_code_2));
+    }
+
     /**
      * 触发提交按钮状态更新
      */
     void toggleSubmitBtnStatus() {
         // 两次密码输入一致时才会触发状态变更
-        boolean condition = !TextUtils.isEmpty(et_mobile.getText()) && !TextUtils.isEmpty(et_verification_code.getText());
+        boolean condition = !TextUtils.isEmpty(et_mobile.getText()) && !TextUtils.isEmpty(et_verificationCode.getText());
         bt_submit.setEnabled(condition);
         bt_submit.getBackground().setAlpha(condition ? 255 : 100);
     }
