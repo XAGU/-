865620ff979e3e7e6fa43f912852d6bfe48452b1
caidong <caidong@youzan.com>
2017-09-07 19:24:56 +0800

    我的钱包

--- app/src/main/java/com/xiaolian/amigo/activity/BaseActivity.java
+++ app/src/main/java/com/xiaolian/amigo/activity/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.xiaolian.amigo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import com.xiaolian.amigo.R;
@@ -15,5 +16,12 @@ public class BaseActivity extends AppCompatActivity {
     @OnClick(R.id.iv_back)
     void back() {
         finish();
+    }
+
+    // 启动activity完成跳转
+    protected void startActivity(AppCompatActivity activity, Class<?> clazz) {
+        Intent intent = new Intent();
+        intent.setClass(activity, clazz);
+        startActivity(intent);
     }
 }
