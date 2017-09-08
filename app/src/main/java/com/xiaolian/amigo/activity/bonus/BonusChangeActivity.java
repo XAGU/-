dbfaf26efa8bab629c358475c4e6a46223436826
caidong <caidong@youzan.com>
2017-09-08 19:32:22 +0800

    红包兑换

---
+++ app/src/main/java/com/xiaolian/amigo/activity/bonus/BonusChangeActivity.java
@@ -0,0 +1,34 @@
+package com.xiaolian.amigo.activity.bonus;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.xiaolian.amigo.R;
+import com.xiaolian.amigo.activity.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by guoyi on 2017/9/6.
+ */
+
+public class BonusChangeActivity extends BaseActivity {
+
+
+    @BindView(R.id.et_change_code)
+    EditText et_changeCode;
+
+    @BindView(R.id.bt_submit)
+    Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bonus_exchange);
+        ButterKnife.bind(this);
+    }
+
+}
