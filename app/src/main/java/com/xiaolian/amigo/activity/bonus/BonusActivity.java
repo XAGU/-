3392454a2bd7e97610638cb5e3e528fa9883e219
caidong <caidong@youzan.com>
2017-09-10 21:25:26 +0800

    加入PullToRefresh控件

--- app/src/main/java/com/xiaolian/amigo/activity/bonus/BonusActivity.java
+++ app/src/main/java/com/xiaolian/amigo/activity/bonus/BonusActivity.java
@@ -1,13 +1,15 @@
 package com.xiaolian.amigo.activity.bonus;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.ListView;
-import android.widget.ScrollView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.xiaolian.amigo.R;
 import com.xiaolian.amigo.activity.BaseActivity;
 import com.xiaolian.amigo.activity.bonus.adaptor.BonusAdaptor;
-import com.xiaolian.amigo.activity.wallet.adaptor.PrepayAdaptor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,7 @@ import butterknife.OnClick;
  * <p>
  * Created by caidong on 2017/9/8.
  */
-public class BonusActivity extends BaseActivity {
+public class BonusActivity extends BaseActivity implements PullToRefreshBase.OnRefreshListener2 {
 
     static List<BonusAdaptor.Bonus> bonuses = new ArrayList<BonusAdaptor.Bonus>() {
         {
@@ -35,6 +37,20 @@ public class BonusActivity extends BaseActivity {
             add(new BonusAdaptor.Bonus(1, 1, "xxxx", "yyyy", 3));
             add(new BonusAdaptor.Bonus(1, 1, "xxxx", "yyyy", 3));
             add(new BonusAdaptor.Bonus(1, 1, "xxxx", "yyyy", 3));
+        }
+    };
+
+    PullToRefreshScrollView sv_scrollview;
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    sv_scrollview.onRefreshComplete();
+                    break;
+            }
         }
     };
 
@@ -61,5 +77,15 @@ public class BonusActivity extends BaseActivity {
     @OnClick(R.id.tv_expired_entry)
     void queryExpiredBonus() {
         startActivity(this, ExpiredBonusActivity.class);
+    }
+
+    @Override
+    public void onPullDownToRefresh(PullToRefreshBase refreshView) {
+        mHandler.sendEmptyMessageDelayed(0, 2000);
+    }
+
+    @Override
+    public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+        mHandler.sendEmptyMessageDelayed(0, 2000);
     }
 }
