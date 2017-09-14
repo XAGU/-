3cd09d31eeafcb17410ec7e483db3cb163e1d8cd
adamzfc <adamzfc@gmail.com>
2017-09-14 10:24:15 +0800

    招领发布、失物发布、我的发布页面

---
+++ app/src/main/java/com/xiaolian/amigo/activity/lostandfound/MyPublishActivity.java
@@ -0,0 +1,57 @@
+package com.xiaolian.amigo.activity.lostandfound;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.xiaolian.amigo.R;
+import com.xiaolian.amigo.activity.lostandfound.adaptor.LostAndFoundAdaptor;
+import com.xiaolian.amigo.base.BaseActivity;
+import com.xiaolian.amigo.common.config.SpaceItemDecoration;
+import com.xiaolian.amigo.common.util.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 我的发布页面
+ * @author zcd
+ */
+
+public class MyPublishActivity extends BaseActivity {
+
+    static List<LostAndFoundAdaptor.Info> infos = new ArrayList<LostAndFoundAdaptor.Info>() {
+        {
+            add(new LostAndFoundAdaptor.Info("", "", "", "", ""));
+            add(new LostAndFoundAdaptor.Info("", "", "", "", ""));
+            add(new LostAndFoundAdaptor.Info("", "", "", "", ""));
+            add(new LostAndFoundAdaptor.Info("", "", "", "", ""));
+            add(new LostAndFoundAdaptor.Info("", "", "", "", ""));
+        }
+    };
+
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager manager;
+
+    /**
+     * 发布列表
+     */
+    @BindView(R.id.rv_my_publish)
+    RecyclerView rv_my_publish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_publish);
+        ButterKnife.bind(this);
+        manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        adapter = new LostAndFoundAdaptor(infos);
+        rv_my_publish.addItemDecoration(new SpaceItemDecoration(ScreenUtils.dpToPxInt(this, 10)));
+        rv_my_publish.setLayoutManager(manager);
+        rv_my_publish.setAdapter(adapter);
+
+    }
+}
