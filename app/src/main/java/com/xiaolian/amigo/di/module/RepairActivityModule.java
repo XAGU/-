dfc75060e93d40621a13d8256306e1245ac187ff
caidong <caidong@youzan.com>
2017-09-18 19:23:35 +0800

    设备报修目录初始化

---
+++ app/src/main/java/com/xiaolian/amigo/di/module/RepairActivityModule.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://mindorks.com/license/apache-v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.xiaolian.amigo.di.module;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xiaolian.amigo.data.manager.OrderDataManager;
+import com.xiaolian.amigo.data.manager.intf.IOrderDataManager;
+import com.xiaolian.amigo.di.OrderActivityContext;
+import com.xiaolian.amigo.di.RepairActivityContext;
+import com.xiaolian.amigo.ui.order.OrderPresenter;
+import com.xiaolian.amigo.ui.order.intf.IOrderPresenter;
+import com.xiaolian.amigo.ui.order.intf.IOrderView;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class RepairActivityModule {
+
+    private AppCompatActivity mActivity;
+
+    public RepairActivityModule(AppCompatActivity activity) {
+        this.mActivity = activity;
+    }
+
+    @Provides
+    Context provideContext() {
+        return mActivity;
+    }
+
+    @Provides
+    AppCompatActivity provideActivity() {
+        return mActivity;
+    }
+
+}
---
+++ app/src/main/java/com/xiaolian/amigo/ui/repair/RepairActivity.java
@@ -0,0 +1,7 @@
+package com.xiaolian.amigo.ui.repair;
+
+/**
+ * Created by caidong on 2017/9/18.
+ */
+public class RepairActivity {
+}
