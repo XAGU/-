e8a8707765c1bcec540d17a92a348f1c9f46286b
caidong <caidong@youzan.com>
2017-09-18 16:07:46 +0800

    收藏的设备列表布局

---
+++ app/src/main/java/com/xiaolian/amigo/data/network/model/device/Device.java
@@ -0,0 +1,36 @@
+package com.xiaolian.amigo.data.network.model.device;
+
+import lombok.Data;
+
+/**
+ * 设备信息
+ * <p>
+ * Created by caidong on 2017/9/18.
+ */
+@Data
+public class Device {
+    // 绑定时间
+    private String bindingTime;
+    // 硬件编号
+    private String hardwareNo;
+    // 健康状态 1 - 正常， 2 - 报修中
+    private int healthStatus;
+    // 设备id
+    private int id;
+    // 纬度
+    private int lat;
+    // 位置
+    private String location;
+    // 经度
+    private int lon;
+    // 设备所在宿舍id
+    private int residenceId;
+    // 设备所在学校id
+    private int schoolId;
+    // 设备所在学校名称
+    private String schoolName;
+    // 设备类型，1 - 热水器， 2 - 饮水机
+    private int type;
+    // 设备用途
+    private int usefor;
+}
--- app/src/main/java/com/xiaolian/amigo/ui/favorite/FavoriteActivity.java
+++ app/src/main/java/com/xiaolian/amigo/ui/favorite/FavoriteActivity.java
@@ -1,11 +1,70 @@
 package com.xiaolian.amigo.ui.favorite;
 
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+import com.xiaolian.amigo.R;
+import com.xiaolian.amigo.tmp.common.config.RecycleViewDivider;
+import com.xiaolian.amigo.tmp.common.config.SpaceItemDecoration;
+import com.xiaolian.amigo.tmp.common.util.ScreenUtils;
+import com.xiaolian.amigo.ui.favorite.adaptor.FavoriteAdaptor;
+import com.xiaolian.amigo.ui.order.OrderActivity;
+import com.xiaolian.amigo.ui.order.adaptor.OrderAdaptor;
+import com.xiaolian.amigo.util.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by caidong on 2017/9/18.
  */
 
 public class FavoriteActivity extends FavoriteBaseActivity {
 
+    List<FavoriteAdaptor.FavoriteWrapper> favorites = new ArrayList<FavoriteAdaptor.FavoriteWrapper>() {
+        {
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+            add(new FavoriteAdaptor.FavoriteWrapper(1, "xxxxxxx"));
+        }
+    };
+
+    @BindView(R.id.rv_favorites)
+    RecyclerView rv_favorites;
+
+    // 收藏设备recycleView适配器
+    FavoriteAdaptor adaptor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+
+        setUnBinder(ButterKnife.bind(this));
+        getActivityComponent().inject(this);
+
+//        presenter.onAttach(this);
+
+        adaptor = new FavoriteAdaptor(favorites);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv_favorites.addItemDecoration(new RecycleViewDivider(this, RecycleViewDivider.VERTICAL_LIST));
+        rv_favorites.setLayoutManager(manager);
+        rv_favorites.setAdapter(adaptor);
+
+//        presenter.requestNetWork(Constant.PAGE_START_NUM);
+    }
 
     @Override
     protected void setUp() {
