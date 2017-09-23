83cd33c9cf6d59c643624a62f4f7ba6c5c11d09e
caidong <caidong@youzan.com>
2017-09-23 14:28:09 +0800

    蓝牙设备操作封装

--- app/src/main/java/com/xiaolian/amigo/data/manager/BLEDataManager.java
+++ app/src/main/java/com/xiaolian/amigo/data/manager/BLEDataManager.java
@@ -1,16 +1,25 @@
 package com.xiaolian.amigo.data.manager;
 
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.os.ParcelUuid;
 
 import com.polidea.rxandroidble.RxBleClient;
+import com.polidea.rxandroidble.RxBleConnection;
+import com.polidea.rxandroidble.RxBleDevice;
 import com.polidea.rxandroidble.scan.ScanFilter;
 import com.polidea.rxandroidble.scan.ScanResult;
 import com.polidea.rxandroidble.scan.ScanSettings;
+import com.polidea.rxandroidble.utils.ConnectionSharingAdapter;
 import com.xiaolian.amigo.data.manager.intf.IBLEDataManager;
+
+import java.util.List;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by caidong on 2017/9/22.
@@ -18,6 +27,7 @@ import rx.Observable;
 public class BLEDataManager implements IBLEDataManager {
 
     private static final String SERVICE_UUID = "0000fee9-0000-1000-8000-00805f9b34fb";
+    private static final String CHARACTERISTIC_UUID = "d44bc439-abfd-45a2-b575-925416129600";
     private RxBleClient client;
 
     @Inject
@@ -32,5 +42,26 @@ public class BLEDataManager implements IBLEDataManager {
                         // 按照SERVICE_UUID筛选
 //                        .setServiceUuid(ParcelUuid.fromString(SERVICE_UUID))
                         .build());
+    }
+
+    @Override
+    public Observable<RxBleConnection> prepareConnectionObservable(RxBleDevice device, boolean autoConnect) {
+        return device
+                .establishConnection(autoConnect)
+                .takeUntil(PublishSubject.create())
+                .compose(new ConnectionSharingAdapter());
+    }
+
+    @Override
+    public Observable<BluetoothGattCharacteristic> connect(Observable<RxBleConnection> connectionObservable) {
+        return connectionObservable
+                .flatMap(RxBleConnection::discoverServices)
+                .flatMap(rxBleDeviceServices -> rxBleDeviceServices.getCharacteristic(UUID.fromString(CHARACTERISTIC_UUID)));
+    }
+
+    @Override
+    public Observable<byte[]> write(Observable<RxBleConnection> connectionObservable, byte[] inputBytes) {
+        return connectionObservable
+                .flatMap(rxBleConnection -> rxBleConnection.writeCharacteristic(UUID.fromString(CHARACTERISTIC_UUID), inputBytes));
     }
 }
--- app/src/main/java/com/xiaolian/amigo/data/manager/intf/IBLEDataManager.java
+++ app/src/main/java/com/xiaolian/amigo/data/manager/intf/IBLEDataManager.java
@@ -1,5 +1,10 @@
 package com.xiaolian.amigo.data.manager.intf;
 
+import android.bluetooth.BluetoothGattCharacteristic;
+
+import com.polidea.rxandroidble.RxBleConnection;
+import com.polidea.rxandroidble.RxBleDevice;
+import com.polidea.rxandroidble.RxBleDeviceServices;
 import com.polidea.rxandroidble.scan.ScanResult;
 
 import rx.Observable;
@@ -13,4 +18,23 @@ public interface IBLEDataManager {
 
     // 扫描设备
     Observable<ScanResult> scan();
+
+    /**
+     * 提供连接蓝牙设备的Observable，附带连接共享适配器供后续蓝牙操作使用
+     *
+     * @param device      蓝牙设备
+     * @param autoConnect 是否自动连接
+     */
+    Observable<RxBleConnection> prepareConnectionObservable(RxBleDevice device, boolean autoConnect);
+
+    // 连接蓝牙
+    Observable<BluetoothGattCharacteristic> connect(Observable<RxBleConnection> connectionObservable);
+
+    /**
+     * 向蓝牙设备发送数据
+     *
+     * @param connectionObservable 连接句柄
+     * @param inputBytes           待发送的数据
+     */
+    Observable<byte[]> write(Observable<RxBleConnection> connectionObservable, byte[] inputBytes);
 }
