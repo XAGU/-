73f2a6fff794bf4152264b738845179ad278ad2d
adamzfc <adamzfc@gmail.com>
2017-10-13 23:59:52 +0800

    修复默认宿舍bug

--- app/src/main/java/com/xiaolian/amigo/ui/user/adaptor/EditDormitoryAdaptor.java
+++ app/src/main/java/com/xiaolian/amigo/ui/user/adaptor/EditDormitoryAdaptor.java
@@ -52,7 +52,7 @@ public class EditDormitoryAdaptor extends CommonAdapter<EditDormitoryAdaptor.Use
         holder.setText(R.id.tv_edit_dormitory_name, userResidenceWrapper.getResidenceName());
         // 只有一个宿舍时，显示为默认宿舍
         if (userResidenceWrapper.isDefault() || getDatas().size() == 1) {
-            presenter.updateResidenceId(userResidenceWrapper.getResidenceId());
+            presenter.saveDefaultResidenceId(userResidenceWrapper.getResidenceId());
             ((ImageView)holder.getView(R.id.iv_choose)).setImageResource(R.drawable.dot_red);
             ((TextView)holder.getView(R.id.tv_choose)).setText("默认宿舍");
             ((TextView) holder.getView(R.id.tv_choose)).setTextColor(ContextCompat.getColor(context, R.color.colorFullRed));
