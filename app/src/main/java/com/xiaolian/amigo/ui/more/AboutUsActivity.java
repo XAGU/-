4393444e486199a15d59026b135d89f81b9f1f6b
adamzfc <adamzfc@gmail.com>
2017-10-16 19:35:59 +0800

    关于我们页面添加版本号显示

--- app/src/main/java/com/xiaolian/amigo/ui/more/AboutUsActivity.java
+++ app/src/main/java/com/xiaolian/amigo/ui/more/AboutUsActivity.java
@@ -1,7 +1,15 @@
 package com.xiaolian.amigo.ui.more;
 
+import android.widget.TextView;
+
 import com.xiaolian.amigo.R;
 import com.xiaolian.amigo.ui.base.BaseToolBarActivity;
+import com.xiaolian.amigo.util.AppUtils;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 关于我们
@@ -10,14 +18,18 @@ import com.xiaolian.amigo.ui.base.BaseToolBarActivity;
  */
 
 public class AboutUsActivity extends BaseToolBarActivity {
+    @BindView(R.id.tv_version)
+    TextView tv_version;
+
     @Override
     protected void initInject() {
-
     }
 
     @Override
     protected void initView() {
+        setUnBinder(ButterKnife.bind(this));
         setMainBackground(R.color.colorBackgroundWhite);
+        tv_version.setText(getString(R.string.app_name) + " " +AppUtils.getVersion(getApplicationContext()));
     }
 
     @Override
