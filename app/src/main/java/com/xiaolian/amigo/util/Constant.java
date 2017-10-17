7e499823b76c9ff1662b1c0c70a1b65c4a79bece
caidong <caidong@youzan.com>
2017-10-17 19:45:09 +0800

    修改device_token为驼峰  否则nginx会变调

--- app/src/main/java/com/xiaolian/amigo/util/Constant.java
+++ app/src/main/java/com/xiaolian/amigo/util/Constant.java
@@ -22,7 +22,7 @@ public final class Constant {
     public static final String CHINEASE_COLON = "：";
 
     // 测试服务器
-    public static final String SERVER_TEST = "http://116.62.236.67:5081";
+    public static final String SERVER_TEST = "https://api.xiaolian365.com";
 //    public static final String SERVER_TEST = "http://10.0.0.2:5081";
 //    public static final String SERVER_TEST = "http://10.0.0.4:8081";
 //    public static final String SERVER_TEST = "http://10.0.0.2:5081";
