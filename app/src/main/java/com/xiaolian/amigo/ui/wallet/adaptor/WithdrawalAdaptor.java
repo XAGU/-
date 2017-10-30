0df85c4f73131d90bc4b329c55877dbdda06f602
adamzfc <adamzfc@gmail.com>
2017-10-30 12:08:47 +0800

    充值提现记录状态字体颜色修改

--- app/src/main/java/com/xiaolian/amigo/ui/wallet/adaptor/WithdrawalAdaptor.java
+++ app/src/main/java/com/xiaolian/amigo/ui/wallet/adaptor/WithdrawalAdaptor.java
@@ -1,8 +1,10 @@
 package com.xiaolian.amigo.ui.wallet.adaptor;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 
 import com.xiaolian.amigo.R;
+import com.xiaolian.amigo.data.enumeration.RechargeStatus;
 import com.xiaolian.amigo.data.enumeration.WithdrawOperationType;
 import com.xiaolian.amigo.data.enumeration.WithdrawalStatus;
 import com.xiaolian.amigo.data.network.model.dto.response.FundsInListDTO;
@@ -21,14 +23,28 @@ import lombok.Data;
  */
 
 public class WithdrawalAdaptor extends CommonAdapter<WithdrawalAdaptor.WithdrawalWrapper> {
+    private Context context;
     public WithdrawalAdaptor(Context context, int layoutId, List<WithdrawalWrapper> datas) {
         super(context, layoutId, datas);
+        this.context = context;
     }
 
     @Override
     protected void convert(ViewHolder holder, WithdrawalWrapper withdrawalWrapper, int position) {
         holder.setText(R.id.tv_withdrawal, withdrawalWrapper.getTitle());
-        holder.setText(R.id.tv_withdrawal_status, WithdrawalStatus.getWithdrawalStatus(withdrawalWrapper.getStatus()).getDesc());
+        if (withdrawalWrapper.getType() == WithdrawOperationType.WITHDRAW.getType()) {
+            holder.setText(R.id.tv_withdrawal_status,
+                    WithdrawalStatus.getWithdrawalStatus(withdrawalWrapper.getStatus()).getDesc());
+            holder.setTextColor(R.id.tv_withdrawal_status,
+                    ContextCompat.getColor(context,
+                            WithdrawalStatus.getWithdrawalStatus(withdrawalWrapper.getStatus()).getColorRes()));
+        } else {
+            holder.setText(R.id.tv_withdrawal_status,
+                    RechargeStatus.getRechargeStatus(withdrawalWrapper.getStatus()).getDesc());
+            holder.setTextColor(R.id.tv_withdrawal_status,
+                    ContextCompat.getColor(context,
+                            RechargeStatus.getRechargeStatus(withdrawalWrapper.getStatus()).getColorRes()));
+        }
         holder.setText(R.id.tv_withdrawal_time, TimeUtils.millis2String(withdrawalWrapper.getTime()));
     }
 
