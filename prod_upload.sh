7f023531073730db621595d15777fa56e167b7dd
adamzfc <adamzfc@gmail.com>
2017-11-14 16:58:38 +0800

    添加蒲公英上传脚本

---
+++ prod_upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -F "file=@app/build/outputs/apk/prod/release/com.xiaolian.amigo_1.0.0.sign.zipalign.apk" \
+    -F "uKey=50d4e96204e7be0dfcef43829e97703c" \
+    -F "_api_key=b7fb050489c7a93733f8ff4bdb1eb636" \
+    https://www.pgyer.com/apiv1/app/upload
