21febca635fdc127bda636642a3fc309e87f6f8a
adamzfc <adamzfc@gmail.com>
2018-01-11 14:57:26 +0800

    更新上传蒲公英脚本

--- dev_upload.sh
+++ dev_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -gt 0 ];then
     versionName=$1
-    curl -F "file=@app/build/outputs/apk/dev/release/com.xiaolian.amigo_dev_1.0.1.sign.zipalign.apk" \
+    curl -F "file=@app/build/outputs/apk/dev/release/com.xiaolian.amigo_dev_${versionName}.sign.zipalign.apk" \
         -F "uKey=50d4e96204e7be0dfcef43829e97703c" \
         -F "_api_key=b7fb050489c7a93733f8ff4bdb1eb636" \
         https://www.pgyer.com/apiv1/app/upload
