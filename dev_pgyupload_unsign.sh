c1b82829de8d3706e7c5f5bb00aa34d857e72d18
lilin <492101684@qq.com>
2018-04-27 10:36:53 +0800

    上传蒲公英为加固脚本

---
+++ dev_pgyupload_unsign.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -gt 0 ];then
+    versionName=$1
+    curl -F "file=@app/build/outputs/apk/dev/release/com.xiaolian.amigo_dev_${versionName}.apk" \
+        -F "uKey=50d4e96204e7be0dfcef43829e97703c" \
+        -F "_api_key=b7fb050489c7a93733f8ff4bdb1eb636" \
+        https://www.pgyer.com/apiv1/app/upload
+else
+    echo "please input version name"
+fi
+
+
