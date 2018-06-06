0a43c55d49e0bf645a23e86d79bd595e5cc619d0
adamzfc <adamzfc@gmail.com>
2018-06-06 17:32:25 +0800

    添加消费记录引导

--- app/src/main/java/com/xiaolian/amigo/ui/widget/dialog/GuideDialog.java
+++ app/src/main/java/com/xiaolian/amigo/ui/widget/dialog/GuideDialog.java
@@ -2,6 +2,7 @@ package com.xiaolian.amigo.ui.widget.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.view.Gravity;
 import android.view.Window;
@@ -21,10 +22,12 @@ import com.xiaolian.amigo.R;
 public class GuideDialog extends Dialog {
     public static final int TYPE_MAIN = 1;
     public static final int TYPE_HEATER = 2;
-    public static final int TYPE_DISPENER = 3;
+    public static final int TYPE_DISPENSER = 3;
+    public static final int TYPE_WALLET_TIP = 4;
 
     private ImageView ivButton;
     private TextView tvLocation;
+    private TextView tvAmount;
 
     public GuideDialog(@NonNull Context context, int type) {
         super(context, R.style.AlertDialogStyle);
@@ -35,7 +38,7 @@ public class GuideDialog extends Dialog {
         WindowManager.LayoutParams lp = window.getAttributes();
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.height = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.alpha = 0.6f;
+//        lp.alpha = 0.6f;
         window.setAttributes(lp);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         setCanceledOnTouchOutside(true);
@@ -47,11 +50,18 @@ public class GuideDialog extends Dialog {
             case TYPE_MAIN:
                 setContentView(R.layout.dialog_guide_main);
                 break;
+            case TYPE_WALLET_TIP:
+                setContentView(R.layout.dialog_guide_wallet);
+                tvAmount = findViewById(R.id.tv_amount);
+//                findViewById(R.id.rel).setAlpha(1);
+//                getWindow().setBackgroundDrawable(new ColorDrawable(0));
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                break;
             case TYPE_HEATER:
                 setContentView(R.layout.dialog_guide_heater);
                 tvLocation = findViewById(R.id.tv_device_title);
                 break;
-            case TYPE_DISPENER:
+            case TYPE_DISPENSER:
                 setContentView(R.layout.dialog_guide_dispenser);
                 tvLocation = findViewById(R.id.tv_device_title);
                 break;
@@ -60,6 +70,12 @@ public class GuideDialog extends Dialog {
         ivButton.setOnClickListener((v -> {
             dismiss();
         }));
+    }
+
+    public void setAmount(String amount) {
+        if (tvAmount != null) {
+            tvAmount.setText(amount);
+        }
     }
 
     public void setLocation(String location) {
