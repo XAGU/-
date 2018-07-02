9c60ef54317174622cd85a77275a0dd39352b543
adamzfc <adamzfc@gmail.com>
2018-07-02 21:18:45 +0800

    付费使用

--- app/src/main/java/com/xiaolian/amigo/ui/device/bathroom/BuyCodeActivity.java
+++ app/src/main/java/com/xiaolian/amigo/ui/device/bathroom/BuyCodeActivity.java
@@ -1,6 +1,9 @@
 package com.xiaolian.amigo.ui.device.bathroom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.xiaolian.amigo.R;
 import com.xiaolian.amigo.ui.device.bathroom.intf.IBuyCodePresenter;
@@ -16,7 +19,7 @@ import butterknife.ButterKnife;
  * @author zcd
  * @date 18/6/29
  */
-public class BuyCodeActivity extends BathroomBaseActivity implements IBuyCodeView {
+public class BuyCodeActivity extends UseWayActivity implements IBuyCodeView {
 
     @Inject
     IBuyCodePresenter<IBuyCodeView> presenter;
@@ -29,5 +32,41 @@ public class BuyCodeActivity extends BathroomBaseActivity implements IBuyCodeVie
 
         getActivityComponent().inject(this);
         presenter.onAttach(this);
+    }
+
+    @Override
+    protected void setToolbarTitle(TextView textView) {
+        textView.setText("购买编码");
+    }
+
+    @Override
+    protected void setTitle(TextView textView) {
+        textView.setText("购买编码");
+    }
+
+    @Override
+    protected void setToolbarSubTitle(TextView textView) {
+        textView.setText("购买记录");
+        textView.setOnClickListener(v -> onSubtitleClick());
+    }
+
+    @Override
+    protected void setSubTitle(TextView textView) {
+        textView.setText("购买记录");
+        textView.setOnClickListener(v -> onSubtitleClick());
+    }
+
+    @Override
+    protected void setTips(TextView tip1, TextView tip2, TextView tip3, TextView tip4,
+                           TextView tip, RelativeLayout rlTip) {
+        tip1.setText(getString(R.string.buy_code_tip1));
+        tip2.setText(getString(R.string.buy_code_tip2));
+        tip3.setText(getString(R.string.buy_code_tip3));
+        tip4.setText(getString(R.string.buy_code_tip4));
+        tip.setText("编码使用说明");
+    }
+
+    private void onSubtitleClick() {
+        startActivity(new Intent(this, BookingRecordActivity.class));
     }
 }
