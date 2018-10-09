9782898db9100dc4ae869f8a8e6f16bbcf251174
wuchangming <625404879@qq.com>
2018-10-09 19:27:31 +0800

    清理不必要服务器

--- app/src/main/java/com/xiaolian/amigo/ui/user/UserCertificationPresenter.java
+++ app/src/main/java/com/xiaolian/amigo/ui/user/UserCertificationPresenter.java
@@ -1,9 +1,22 @@
 package com.xiaolian.amigo.ui.user;
 
+import com.xiaolian.amigo.data.manager.intf.IUserDataManager;
 import com.xiaolian.amigo.ui.base.BasePresenter;
 import com.xiaolian.amigo.ui.user.intf.IUserCertificationPresenter;
 import com.xiaolian.amigo.ui.user.intf.IUserCertificationView;
 
+import javax.inject.Inject;
+
 public class UserCertificationPresenter <v extends IUserCertificationView> extends BasePresenter<v>
     implements IUserCertificationPresenter<v>{
+
+    private IUserDataManager userDataManager ;
+
+    
+    @Inject
+    public UserCertificationPresenter(IUserDataManager userDataManager ){
+        super();
+        this.userDataManager = userDataManager ;
+
+    }
 }
