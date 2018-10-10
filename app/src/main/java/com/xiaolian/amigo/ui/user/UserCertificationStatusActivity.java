897fe94851f75cb699452eadbc97e793331c9ed9
wuchangming <625404879@qq.com>
2018-10-10 21:17:06 +0800

    添加学生认证状态界面

--- app/src/main/java/com/xiaolian/amigo/ui/user/UserCertificationStatusActivity.java
+++ app/src/main/java/com/xiaolian/amigo/ui/user/UserCertificationStatusActivity.java
@@ -1,20 +1,23 @@
 package com.xiaolian.amigo.ui.user;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.xiaolian.amigo.R;
+import com.xiaolian.amigo.ui.base.BaseActivity;
 import com.xiaolian.amigo.ui.user.intf.IUserCertificationStatusView;
 
-public class UserCertificationStatusActivity  extends UserBaseActivity implements IUserCertificationStatusView{
+public class UserCertificationStatusActivity  extends BaseActivity implements IUserCertificationStatusView{
+
     @Override
-    protected void initView() {
+    protected void setUp() {
 
     }
 
-    @Override
-    protected int setTitle() {
-        return 0;
-    }
 
     @Override
-    protected int setLayout() {
-        return 0;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_certification_status);
     }
 }
