e65fbf107c489db7c9b9f26441d1f5b08badf278
Yangyong <791038832@qq.com>
2019-01-16 10:22:14 +0800

    nb设备开发基础类搭建

---
+++ app/src/main/java/com/xiaolian/amigo/ui/device/nbdevice/NBDeviceActivity.java
@@ -0,0 +1,11 @@
+package com.xiaolian.amigo.ui.device.nbdevice;
+
+import com.xiaolian.amigo.ui.base.BaseActivity;
+import com.xiaolian.amigo.ui.device.nbdevice.intf.INBDeviceView;
+
+public class NBDeviceActivity extends BaseActivity implements INBDeviceView {
+    @Override
+    protected void setUp() {
+
+    }
+}
---
+++ app/src/main/java/com/xiaolian/amigo/ui/device/nbdevice/NBDevicePresenter.java
@@ -0,0 +1,8 @@
+package com.xiaolian.amigo.ui.device.nbdevice;
+
+import com.xiaolian.amigo.ui.base.BasePresenter;
+import com.xiaolian.amigo.ui.device.nbdevice.intf.INBDevicePresenter;
+import com.xiaolian.amigo.ui.device.nbdevice.intf.INBDeviceView;
+
+public class NBDevicePresenter<v extends INBDeviceView> extends BasePresenter<v> implements INBDevicePresenter<v> {
+}
